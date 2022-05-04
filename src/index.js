69624e3227a52d919056565d654349ecf57b433d
Hristo Ganev <hristo.b.ganev@gmail.com>
2022-05-04 20:00:33 +0200

    Add postman for testing

--- src/index.js
+++ src/index.js
@@ -77,6 +77,20 @@ app.post('/tasks', async (req, res) => {
     }
 })
 
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
+
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+
 app.get('/tasks', async (req, res) => {
     try {
         const tasks = await Task.find({})
@@ -123,6 +137,21 @@ app.patch('/tasks/:id', async (req, res) => {
         res.status(400).send(error)
     }
 
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+
+        if (!task) {
+            return  res.status(404).send()
+        }
+
+        res.send(task)
+
+    } catch (error) {
+        res.status(500).send()
+    }
 })
 
 app.listen(port, () => {
