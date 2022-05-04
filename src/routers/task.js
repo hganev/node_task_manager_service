d273037a3bc33b64ef3e7f195fd9fac5bce0ed50
Hristo Ganev <hristo.b.ganev@gmail.com>
2022-05-04 20:14:28 +0200

    Extract and split routes in separate files

---
+++ src/routers/task.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const Task = require('../models/task.js')
+
+const router = new express.Router()
+
+router.post('/tasks', async (req, res) => {
+    const task = new Task(req.body)
+
+    try {
+        await task.save()
+        res.status(201).send(task)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+router.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
+
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+
+router.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find({})
+        res.send(tasks)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+router.get('/tasks/:id', async (req, res) => {
+    const _id = req.params.id
+
+    try {
+        const task = await Task.findById(_id)
+        if (!task) {
+            res.status(404).send()
+        } else { 
+            res.send(task)
+        }
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+router.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    
+    const isValidOperation = updates.every(update => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        res.status(400).send({ error: 'Invalid updates found!'})
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+
+})
+
+router.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+
+        if (!task) {
+            return  res.status(404).send()
+        }
+
+        res.send(task)
+
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+
+module.exports = exports = router
