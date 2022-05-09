e8b7cc3ae70af6e7366ef913abf910ba3e2fa4a5
Hristo Ganev <hristo.b.ganev@gmail.com>
2022-05-09 18:46:08 +0200

    Add jwt package

--- src/index.js
+++ src/index.js
@@ -14,3 +14,15 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
+
+// const jwt = require('jsonwebtoken')
+
+// const myfuction = async () => {
+//     const token = jwt.sign({ _id: '12345' }, 'mynameishristo', { expiresIn: '7 days'})
+//     console.log('token', token)
+
+//     const payload = jwt.verify(token, 'mynameishristo')
+//     console.log('payload', payload)
+// }
+
+// myfuction()
